00ded87e07fa590f15c83dfe03f26ea4973e33be
Huy on HW <huyluong82@googlemail.com>
2024-09-10 15:20:15 +0700

    Add output to log

--- update.sh
+++ update.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
 
 if grep -q "Arch" $release_file
 then
-    sudo pacman -Syu
+    sudo pacman -Syu 1>>$logfile 2>>$errorlog
 fi
 if grep -q "Ubuntu" $release_file || grep -q "Debian" $release_file
 then
-    sudo apt update
-    sudo apt upgrade -y
+    sudo apt update 1>>$logfile 2>>$errorlog
+    sudo apt upgrade -y 1>>$logfile 2>>$errorlog
 fi
