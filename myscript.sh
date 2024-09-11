3ea1f60b0537b0312194e476efa372ed1b31a552
Huy on HW <huyluong82@googlemail.com>
2024-09-11 15:46:08 +0700

    Add case

--- myscript.sh
+++ myscript.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-echo "This is a case script"
+finished=0
+
+while [ $finished -ne 1 ]
+do
+    echo "What is your favourite Linux distribution?"
+
+    echo "1 - Arch"
+    echo "2 - CentOS"
+    echo "3 - Debian"
+    echo "4 - Mint"
+    echo "5 - Ubuntu"
+    echo "6 - Something else.."
+    echo "7 - Exit"
+
+    read distro;
+
+    case $distro in
+        1) echo "Arch is a rolling relaese.";;
+        2) echo "CentOS is popular on servers.";;
+        3) echo "Debian is a community distribution.";;
+        4) echo "Mint is popular on desktops and laptops.";;
+        5) echo "Ubuntu is popular on both servers and computers.";;
+        6) echo "There are many distributions out there.";;
+        7) finished=1;;
+        *) echo "You didn't enter an approriate choice."
+    esac
+done
+
+echo "Thank you for using this script."
