9e2eb080cc7bcc10b83dbbf948d3ac54cdfed7d7
Huy on HW <huyluong82@googlemail.com>
2024-09-13 14:15:21 +0700

    Add content

--- myscript.sh
+++ myscript.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "This is a bash script"
+lines=$(ls -lh $1 | wc -l)
+
+if [ $# -ne 1 ]
+then
+    echo "This script requires exactly one directory path passed to it."
+    echo "Please try again."
+    exit 1
+fi
+echo "You have $(($lines-1)) objects in the $1 directory."
